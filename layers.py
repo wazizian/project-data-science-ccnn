b84d14de1678b89ffefc1233604ecf36fd96a696
Waïss Azizian <waissfowl@gmail.com>
2019-12-09 19:24:46 +0100

    Add patch extraction

--- layers.py
+++ layers.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+from kernels import ApproxKernelMachine
+import numpy as np
+import sklearn.feature_extraction.image as image
 
 class CCNNLayerLinear(nn.Module):
     def __init__(self, m: int, P: int, d2: int, R: float):
@@ -32,7 +35,22 @@ class CCNNLayerLinear(nn.Module):
             raise ValueError("Unsupported norm")
 
 
-
-
-
+class CCNNLayer(nn.Module):
+    def __init__(self, m, P, d2, R, patch_dim, patch_stride, kernel, gamma=0.2):
+        super(CCNNLayer, self).__init__()
+        self.kernel = ApproxKernelMachine(kernel, m, gamma=gamma)
+        self.linear = CCNNLayerLinear(m, P, d2, R)
+        self.patch_dim = patch_dim
+        self.patch_stride = patch_size
+    
+    #WARNING: the rest is maybe not made for multichannels
+    def extract_patches(self, imgs: torch.Tensor):
+        """
+        imgs has shape (b, c, h, w)
+        output has shape (n_samples, P, d1) where d1 = self.patch_dim**2
+        """
+        (b, c, h, w) = imgs.size()
+        patches = imgs.unfold(2, self.patch_dim, self.patch_stride).unfold(3, self.patch_dim, self.patch_stride)
+        patches = patches.view(b, self.P, self.patch_dim**2)
+        return patches
 
