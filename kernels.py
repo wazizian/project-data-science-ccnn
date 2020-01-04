6ca8ee614ae4622a1b7a98d68d39aa596eb35938
Waïss Azizian <waissfowl@gmail.com>
2020-01-04 14:41:49 +0100

    Add RBF kernel approximation

--- kernels.py
+++ kernels.py
@@ -10,7 +10,7 @@ class ApproxKernelMachine:
         """
         self.m = m
         if kernel == 'rbf':
-            self.kernel_approx = kernel_approximation.Nystroem(kernel='rbf', gamma=gamma, n_components=m)
+            self.kernel_approx = kernel_approximation.RBFSampler(gamma=gamma, n_components=m)
         elif kernel == 'polynomial':
             self.kernel_approx = kernel_approximation.Nystroem(kernel='polynomial', gamma=-1, coef0=2, degree=-1, n_components=m)
         else:
@@ -28,7 +28,7 @@ class ApproxKernelMachine:
     def buid_kernel_patch_dataset(self, labels: torch.Tensor) -> data.Dataset:
         assert labels.size() == (self.n,)
         Z = np.reshape(self.Q, (self.n, self.P, self.m))
-        Z_tensor = torch.from_numpy(Z)
+        Z_tensor = torch.from_numpy(Z).float()
         dataset = data.TensorDataset(Z_tensor, labels)
         print("Built kernel matrix as dataset with {} samples of shape {}x{}".format(len(dataset), self.P, self.m))
         return dataset
@@ -41,7 +41,7 @@ class ApproxKernelMachine:
         assert X.shape[1] == self.P and X.shape[2] == self.d1
         b = X.shape[0]
         transformed = self.kernel_approx.transform(np.reshape(X, (b*self.P, self.d1)))
-        return torch.from_numpy(np.reshape(transformed, (b, self.P, self.m))) 
+        return torch.from_numpy(np.reshape(transformed, (b, self.P, self.m))).float()
 
         
         
